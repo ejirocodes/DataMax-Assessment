952ff6dd6f8dab56dd6ba6d3f3e01c761ad6201c
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2021-03-26 03:57:01 +0100

    paginate data

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -5,7 +5,7 @@
     <v-card class="card-table">
       <v-row class="px-4 pt-4 pb-2">
         <v-spacer></v-spacer>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="4">
           <v-text-field
             v-model="search"
             outlined
@@ -13,7 +13,7 @@
             dense
             hide-details
             append-icon="mdi-magnify"
-            label="Search for attendees..."
+            label="Search for Books..."
             class="ma-2"
           ></v-text-field>
         </v-col>
@@ -29,7 +29,6 @@
   </v-container>
 </template>
 <script>
-import countries from "../data/countries";
 import PreLoader from "../components/PreLoader";
 
 export default {
@@ -50,12 +49,6 @@ export default {
         { text: "Released", value: "released" },
       ],
       books: [],
-      countries,
-      nationality: null,
-      findNationality: false,
-      text: "",
-      timeout: 3500,
-      color: "sec",
       isLoading: true,
     };
   },
@@ -65,71 +58,20 @@ export default {
   methods: {
     async getBooks() {
       try {
-        const res = await fetch(`https://www.anapioficeandfire.com/api/books?page=1`);
+        const res = await fetch(
+          `https://www.anapioficeandfire.com/api/books?pageSize=12`
+        );
         const data = await res.json();
-        console.log(data);
-        this.books = data
+        console.log({ data });
+        this.books = data;
+        this.isLoading = false;
       } catch (error) {
         console.log(error);
       }
     },
-    async guessNationality(item) {
-      try {
-        const res = await fetch(`https://api.nationalize.io?name=${item.name}`);
-        const data = await res.json();
-        let nationalities = data.country;
-
-        // If the country array is empty, notify the user (vendor)
-        if (nationalities.length <= 0) {
-          this.color = "error";
-          this.findNationality = true;
-          this.text = `Oops! we could not user find ${item.name} Nationality! 😭`;
-          return false;
-        }
-
-        // Get probability of countries and add them to a single array
-        const probability = [
-          ...new Set(nationalities.map((it) => it.probability)),
-        ];
-        // Get the country with higest probability
-        let maxProbality = Math.max(...probability);
-
-        // Find the country from the attendees array based on the highest probability
-        const matchedCountry = nationalities.find(
-          (nat) => nat.probability === maxProbality
-        );
-
-        // Get the country name based on the country ID retured from attendees.json
-        let matchedNationName = this.countries.find(
-          (country) =>
-            matchedCountry.country_id.toLowerCase() === country.alpha2
-        );
-
-        // If all criteria are met,
-        // show toast with the prdicted user Nationality
-        this.findNationality = true;
-        this.color = "sec";
-        this.text = `${item.name} is from ${matchedNationName.name}`;
-      } catch (error) {
-        this.findNationality = true;
-        this.text = `Something went wrong! Please try again 🙏🏾`;
-        this.color = "error";
-      }
-    },
-    getColor(gender) {
-      if (gender === "Male") {
-        return "sec";
-      } else return "primary";
-    },
-  },
-  beforeCreate() {
-    this.isLoading = true;
   },
   mounted() {
-    this.getBooks()
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 1400);
+    this.getBooks();
   },
 };
 </script>
